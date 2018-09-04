8f6da8059557476c773f77be398947cb7ddb9b06
Masa171201 <obara@chowagiken.co.jp>
2018-09-04 11:49:45 +0900

    ブランチにてsubを追加

--- sub.php
+++ sub.php
@@ -8,12 +8,8 @@
 <?php
 
 $array_shop = array();
-$array_shop[0] = '札幌';
-$array_shop[1] = '旭川';
-$array_shop[2] = '函館';
-$array_shop[3] = '名古屋';
-$array_shop[4] = '松戸';
-$array_shop[5] = '習志野';
+$array_shop[0] = '熊本';
+$array_shop[1] = '福岡';
 
 print_r( $array_shop );
 print('<br>');
