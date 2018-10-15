613835773a025995d0b8fd6d2fca794230b5c995
Masa171201 <obara@chowagiken.co.jp>
2018-10-15 15:22:59 +0900

    SubFolder Created.

---
+++ sample/sub02.php
@@ -0,0 +1,22 @@
+﻿<html>
+<head>
+<title>PHP TEST</title>
+</head>
+
+<body>
+
+<?php
+
+$array_shop = array();
+$array_shop[0] = '発寒';
+$array_shop[1] = '八軒';
+
+print_r( $array_shop );
+print('<br>');
+print_r( count( $array_shop ) );
+
+
+?>
+
+</body>
+</html>
